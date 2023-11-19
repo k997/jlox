d4b34f5455b54a8aff1f84f20e45858fd7d1007d
kong <26889548+k997@users.noreply.github.com>
2023-11-19 09:31:49 +0800

    抽象语法树生成工具支持生成带超类的类语句, 支持生成 super 表达式

--- src/com/craftinginterpreters/tool/GenerateAst.java
+++ src/com/craftinginterpreters/tool/GenerateAst.java
@@ -30,6 +30,7 @@ public class GenerateAst {
                 "Get : Expr object, Token name",
                 "Set : Expr object, Token name, Expr value",
                 "This : Token keyword",
+                "Super : Token keyword, Token method",
                 "Grouping : Expr expression",
                 "Literal  : Object value",
                 "Logical : Expr left, Token operator, Expr right",
@@ -37,7 +38,7 @@ public class GenerateAst {
                 "Variable : Token name"));
         defineAst(outputDir, "Stmt", Arrays.asList(
                 "Block : List<Stmt> statements",
-                "Class : Token name, List<Stmt.Function> methods",
+                "Class : Token name, Expr.Variable superclass, List<Stmt.Function> methods",
                 "Expression : Expr expression",
                 "Function : Token name, List<Token> params, List<Stmt> body",
                 "Return : Token keyword, Expr value",
